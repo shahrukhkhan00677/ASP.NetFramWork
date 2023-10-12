e42b4950efedeb617eb50113833bc9dada5af8c2
shahrukhkhan00677 <shahrukhkhan00677@gmail.com>
2023-10-12 09:54:00 +0530

    update

--- Linq/EmployeeDetails.cs
+++ Linq/EmployeeDetails.cs
@@ -8,14 +8,16 @@ using static System.Formats.Asn1.AsnWriter;
 namespace LinqStudy
 {
     //LINQ = Language Integrated Query
-    //LINQ two type => 1. Query Experision , 2. Function
+    //LINQ two type
+    //1. Query Experision
+    //2. Function
     internal class EmployeeDetails
     {
-        List<Employee> employees=new List<Employee>();
+        List<Employee>employees=new List<Employee>();
         int[] scores = {92,87,75,68,53 };
         public EmployeeDetails()
         {
-            
+            #region Department 
 
             Department department1 = new Department();
             department1.Id = 1;
@@ -30,6 +32,11 @@ namespace LinqStudy
             department1.OfficeList.Add("Mumbai");
             department1.OfficeList.Add("Banglor");
 
+            #endregion
+
+
+            #region Employees
+
             Employee objemp1 = new Employee();
             objemp1.Id = 1;
             objemp1.Name = "Shahrukh";
@@ -42,14 +49,22 @@ namespace LinqStudy
             objemp2.Name = "Ravi";
             objemp2.Salary = 20;
             objemp2.EmpDepartment = department2;
-
             employees.Add(objemp2);
+
+            Employee objemp3 = new Employee();
+            objemp3.Id = 3;
+            objemp3.Name = "Sagar";
+            objemp3.Salary = 20;
+            objemp3.EmpDepartment = department2;
+            employees.Add(objemp3);
+
+            #endregion
         }
         public void GetScroreGraterthan80() 
         {
             #region WhereFunctionOfLinq
 
-            // Query Experision.......
+            //1. Query Experision....................................................
 
             /* IEnumerable<int> scoreQuery = from score in scores where score < 80 select score;
              foreach (int score in scoreQuery) 
@@ -57,7 +72,11 @@ namespace LinqStudy
                  Console.WriteLine(score);
              }*/
 
-            // through function ...........
+
+
+
+
+            //2. Through function ......................................................
 
             /*var result = scores.Where(x => x > 80);
             foreach (int score in result) 
@@ -88,38 +107,158 @@ namespace LinqStudy
 
             // var listOfId=employees.Select(x => new { x.Id , x.Name });
 
-           /* var listOfId = employees
-                .Where(x=>x.Salary>10)
-                .Select(x => new { x.Id, x.Name });
+            /* var listOfId = employees
+                 .Where(x=>x.Salary>10)
+                 .Select(x => new { x.Id, x.Name });
 
-            foreach (var item in listOfId)
-            {
-                Console.WriteLine(item.Id);
-                Console.WriteLine(item.Name);
+             foreach (var item in listOfId)
+             {
+                 Console.WriteLine(item.Id);
+                 Console.WriteLine(item.Name);
 
-            }*/
+             }*/
 
-           /* var empDepartment = employees.Select(x => new { x.EmpDepartment });
-            foreach (var item in empDepartment)
-            {
+            /* var empDepartment = employees.Select(x => new { x.EmpDepartment });
+             foreach (var item in empDepartment)
+             {
 
-                 Console.WriteLine("empDeparment : " + item.EmpDepartment.Name);
-                foreach (var office in item.EmpDepartment.OfficeList)
-                {
+                  Console.WriteLine("empDeparment : " + item.EmpDepartment.Name);
+                 foreach (var office in item.EmpDepartment.OfficeList)
+                 {
 
-                    Console.WriteLine("OfficeName :"+ office);
-                }
-            }*/
+                     Console.WriteLine("OfficeName :"+ office);
+                 }
+             }*/
 
-           //select many  
+            //select many  
 
-            var newEmpDepartment=employees.SelectMany(x=> x.EmpDepartment.OfficeList);
+            /*var newEmpDepartment=employees.SelectMany(x=> x.EmpDepartment.OfficeList);
             foreach (var item in newEmpDepartment)
             {
                 Console.WriteLine(item);
-            }
+            }*/
 
             #endregion
+
+            #region FirstAndDefault
+
+            /* try
+             {
+                 var Emp = employees.First(x => x.Salary == 50);  // first name print of where salary = 20
+                 Console.WriteLine(Emp.Name);
+             }
+             catch (Exception ex)
+             {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.Message+" (not found data) ");
+
+
+            }*/
+
+
+
+            // **********************************************************************************************//
+
+
+
+            /* try
+             {
+                 var Emp = employees.FirstOrDefault(x => x.Salary == 50);  // first name print of where salary = 20
+                 if (Emp != null)
+                 {
+                     Console.WriteLine(Emp.Name);
+                 }
+                 else
+                 {
+                     Console.WriteLine("Record not found");
+                 }
+             }
+             catch (Exception ex)
+             {
+                 Console.WriteLine(ex.Message);
+             }*/
+            #endregion
+
+            #region LastAndDefault
+            /* try
+             {
+                 var emp = employees.LastOrDefault(x => x.Salary == 20);
+                 if (emp != null)
+                 {
+                     Console.WriteLine(emp.Name);
+                 }
+                 else 
+                 { Console.WriteLine("record not found");
+                 }
+
+             }
+             catch (Exception ex)
+             {
+                 Console.WriteLine(ex.Message);
+
+
+             }*/
+            #endregion
+
+            #region SingleAndDefault
+            /* var emp=employees.SingleOrDefault(x => x.Salary == 20);
+             if (emp!=null)
+             {
+                 Console.WriteLine(emp.Name);
+
+             }
+             else
+             {
+                 Console.WriteLine("Single record not have");
+             }*/
+
+
+
+            /*  try
+              {
+                  var emp = employees.SingleOrDefault(x => x.Salary == 20);
+                  if (emp != null)
+                  {
+                      Console.WriteLine(emp.Name);
+
+                  }
+
+              }
+              catch (Exception ex)
+              {
+                  Console.WriteLine(ex.Message);
+
+
+              }*/
+
+
+            #endregion
+
+            #region AlAndlAny
+            //any return bool value
+            //any use to find the name start with letter (.....)
+
+
+            /* var empName=employees.Any(x => x.Name.StartsWith("S"));
+             Console.WriteLine(empName);*/
+
+            //All are use to all employee name start "S" than value is true other wise value is false 
+
+            /*   var empall=employees.All(x => x.Name.StartsWith("S"));
+               Console.WriteLine(empall);*/
+            #endregion
+
+            #region ElementAtGroupBy
+
+            /*var element=employees.ElementAt(0);
+            Console.WriteLine(element.Name);*/
+
+            #endregion
+
+            #region GroupBy
+
+
+            #endregion  
         }
     }
 }
